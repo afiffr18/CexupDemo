8ed3aaecf3d1958a37065601c68f4159180d9eef
Afif Fujia Rahman <afiffujiarahman2000@gmail.com>
2023-06-20 18:07:46 +0700

    fix error different java version

--- app/src/main/java/com/example/democexup/local/Entities/Mahasiswa.kt
+++ app/src/main/java/com/example/democexup/local/Entities/Mahasiswa.kt
@@ -1,7 +1,8 @@
 package com.example.democexup.local.Entities
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
-
+@Entity
 data class Mahasiswa(
     @PrimaryKey(autoGenerate = false)
     val nim:String,
