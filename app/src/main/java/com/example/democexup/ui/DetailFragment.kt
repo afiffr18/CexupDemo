b1c06a44224c4f9ebb462325c3aa61d9c82eddbf
Afif Fujia Rahman <afiffujiarahman2000@gmail.com>
2023-06-21 10:20:43 +0700

    Menambahkan search pada halaman DetailFragment

--- app/src/main/java/com/example/democexup/ui/DetailFragment.kt
+++ app/src/main/java/com/example/democexup/ui/DetailFragment.kt
@@ -1,11 +1,17 @@
 package com.example.democexup.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,11 +20,13 @@ import com.example.democexup.R
 import com.example.democexup.databinding.FragmentDetailBinding
 import com.example.democexup.domain.repository.DemoRepository
 import com.example.democexup.local.Dao.DatabaseDemo
+import com.example.democexup.local.Entities.Mahasiswa
+import java.util.Locale
 
 
 class DetailFragment : Fragment() {
     private lateinit var detailAdapter: DetailAdapter
-
+    lateinit var courseList: ArrayList<Mahasiswa>
     private val demoRepository : DemoRepository by lazy { DemoRepository(DatabaseDemo.getInstance(requireContext())!!) }
     private val demoViewModel : DemoViewModel by lazy { DemoViewModel(demoRepository) }
     private var _binding : FragmentDetailBinding? = null
@@ -27,6 +35,7 @@ class DetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true);
         // Inflate the layout for this fragment
         _binding = FragmentDetailBinding.inflate(inflater,container,false)
         return binding.root
@@ -40,6 +49,7 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        courseList = ArrayList()
         val nid = arguments?.getString("nid")!!
         val nama = arguments?.getString("nama")!!
         val makulId = arguments?.getInt("matakuliah_id")!!
@@ -49,6 +59,7 @@ class DetailFragment : Fragment() {
 
         initRecycler()
         getMatakuliahData(makulId)
+        updateAdapter()
     }
 
     private fun initRecycler(){
@@ -67,12 +78,79 @@ class DetailFragment : Fragment() {
                 }
 
                 Status.SUCCESS ->{
-                    it.data?.let { data -> detailAdapter.updateMahasiswa(data.component1().mahasiswa) }
+                    it.data?.let { data ->
+                        updateData(data.component1().mahasiswa)
+                    }
                 }
                 Status.ERROR ->{
                     Toast.makeText(requireContext(),"Gagal Memuat Data", Toast.LENGTH_LONG).show()
                 }
             }
+        }
+    }
+
+    private fun updateData(list : List<Mahasiswa>){
+        detailAdapter.updateMahasiswa(list)
+        Handler(Looper.getMainLooper()).postDelayed({
+            courseList.addAll(list)
+        },500)
+    }
+    private fun updateAdapter(){
+        detailAdapter.updateMahasiswa(courseList)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        // below line is to get our inflater
+
+        // inside inflater we are inflating our menu file.
+        inflater.inflate(R.menu.search_menu, menu)
+
+        // below line is to get our menu item.
+        val searchItem: MenuItem = menu.findItem(R.id.actionSearch)
+
+        // getting search view of our item.
+        val searchView: SearchView = searchItem.getActionView() as SearchView
+
+        // below line is to call set on query text listener method.
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            android.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(msg: String): Boolean {
+                // inside on query text change method we are
+                // calling a method to filter our recycler view.
+                filter(msg)
+                return false
+            }
+        })
+    }
+
+
+    private fun filter(text: String) {
+        // creating a new array list to filter our data.
+        val filteredlist: ArrayList<Mahasiswa> = ArrayList()
+
+        // running a for loop to compare elements.
+        for (item in courseList) {
+            // checking if the entered string matched with any item of our recycler view.
+            if (item.nama.lowercase(Locale.ROOT).contains(text.lowercase(Locale.ROOT)) || item.nim.lowercase(Locale.ROOT).contains(text.lowercase(Locale.ROOT)) ) {
+                // if the item is matched we are
+                // adding it to our filtered list.
+                filteredlist.add(item)
+            }
+        }
+        if (filteredlist.isEmpty()) {
+            // if no item is added in filtered list we are
+            // displaying a toast message as no data found.
+            Toast.makeText(requireContext(), "No Data Found..", Toast.LENGTH_SHORT).show()
+        } else {
+            // at last we are passing that filtered
+            // list to our adapter class.
+            detailAdapter.updateMahasiswa(filteredlist)
         }
     }
 
